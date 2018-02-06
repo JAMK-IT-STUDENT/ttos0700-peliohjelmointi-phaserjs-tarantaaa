259d0de3419a4dbaa21a22f4d7789ba383445c1f
Tarleena Marttila <earlybird87@gmail.com>
2018-02-06 13:23:33 +0200

    Udemy - Making games with Phaser. State 0 Add background and camera movement. Changed the character to one which is facing to right.

--- README.md
+++ README.md
@@ -4,5 +4,10 @@ PhaserJS exercise related to game programming course by JAMK.
 The instructions: https://www.udemy.com/making-games-with-phaser/
 
 
-Sprites: https://opengameart.org/content/animal-pack-redux
+Sprites: 
+
+https://opengameart.org/content/animal-pack-redux
+License: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
+
+https://opengameart.org/content/several-scrolling-backgrounds-and-layerable-runners
 License: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
--- state0.js
+++ state0.js
@@ -1,30 +1,54 @@
-var demo = {}, centerX = 1500 / 2, centerY = 1000 / 2, rabbit, speed = 4;
+var demo = {}, highMax = 695, highMin = 826, centerX = 1500 / 2, centerY = 800, runner, rabbit, speed = 6;
 demo.state0 = function(){};
 demo.state0.prototype = {
     preload: function(){
+        game.load.image('runner', 'assets/sprites/female_runner.png');
         game.load.image('rabbit', 'assets/sprites/rabbit.png');
+        game.load.image('desert', 'assets/backgrounds/desert_BG.png');
     },
     create: function(){
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         game.stage.backgroundColor = '#80ff80';
         console.log('state0');
         addStateEventListeners();
+        game.world.setBounds(0, 0, 2958, 1000);
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        var desertBG = game.add.sprite(0, 0, 'desert');
         
-        rabbit = game.add.sprite(centerX, centerY, 'rabbit');
-        rabbit.anchor.setTo(0.5, 0.5);
+        rabbit = game.add.sprite(centerX + 260, centerY - 200, 'rabbit');
+        rabbit.scale.setTo(0.6, 0.6);
+        
+        runner = game.add.sprite(centerX, centerY - 40, 'runner');
+        runner.anchor.setTo(0.5, 0.5);
+        game.physics.enable(runner);
+        runner.body.collideWorldBounds = true;
+        
+        
+        
+        game.camera.follow(runner);
+        game.camera.deadzone = new Phaser.Rectangle(centerX - 300, 0, 600, 1000);
     },
     update: function(){
         if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-            rabbit.x += speed;
+            runner.scale.setTo(1, 1);
+            runner.x += speed;
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
-            rabbit.x -= speed;
+            runner.scale.setTo(-1, 1);
+            runner.x -= speed;
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
-            rabbit.y -= speed;
+            runner.y -= speed;
+            if(runner.y < highMax){
+                runner.y = highMax;
+            }
+            
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
-            rabbit.y += speed;
+            runner.y += speed;
+            if(runner.y > highMin){
+                runner.y = highMin;
+            }
         }
     }
 };
