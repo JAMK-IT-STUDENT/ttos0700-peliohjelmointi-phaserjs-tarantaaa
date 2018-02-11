8926afcadd2596d71ffd7066cb02ecf1a8bfb9d7
Tarleena Marttila <earlybird87@gmail.com>
2018-02-11 16:25:21 +0200

    Udemy - Making games with phaser. State 1 Added collision between character and tile map.

--- state1.js
+++ state1.js
@@ -1,4 +1,7 @@
 demo.state1 = function(){};
+
+var cursors, vel=500, rocks, dunes;
+
 demo.state1.prototype = {
     preload: function(){
         game.load.tilemap('field', 'assets/tilemaps/field.json', null, Phaser.Tilemap.TILED_JSON);
@@ -6,6 +9,7 @@ demo.state1.prototype = {
         game.load.image('dunes_tileset', 'assets/tilemaps/dunes_tileset.png');
     },
     create: function(){
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         game.stage.backgroundColor = '#DDDDDD';
         addStateEventListeners();
         
@@ -13,9 +17,36 @@ demo.state1.prototype = {
         map.addTilesetImage('dunes_tileset');
         map.addTilesetImage('dland_tileset');
         
-        var dunes = map.createLayer('dunes');
-        var rocks = map.createLayer('rocks');
+        dunes = map.createLayer('dunes');
+        rocks = map.createLayer('rocks');
         
+        map.setCollisionBetween(3, 11, true, 'rocks');
+        
+        runner = game.add.sprite(200, 200, 'rabbitsheet');
+        game.physics.enable(runner);
+        
+        cursors = game.input.keyboard.createCursorKeys();
     },
-    update: function(){}
+    update: function(){
+        game.physics.arcade.collide(runner, rocks);
+        
+        if(cursors.up.isDown){
+            runner.body.velocity.y = -vel;
+        }
+        else if(cursors.down.isDown){
+            runner.body.velocity.y = vel;
+        }
+        else{
+            runner.body.velocity.y = 0;
+        }
+        if(cursors.left.isDown){
+            runner.body.velocity.x = -vel;
+        }
+        else if(cursors.right.isDown){
+            runner.body.velocity.x = vel;
+        }
+        else{
+            runner.body.velocity.x = 0;
+        }
+    }
 };
